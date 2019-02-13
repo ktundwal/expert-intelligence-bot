5bc90bcf347591e2ef8a092f696ca4ea415b3be6
Kapil Tundwal <katundwa@microsoft.com>
2019-02-13 10:06:17 -0800

    initial checkin. base code was downloaded from Azure Bot Service after deploying a template.

---
+++ ppt-expert-connect/Dialogs/Greeting/GreetingState.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace com.microsoft.ExpertConnect
+{
+    /// <summary>
+    /// User state properties for Greeting.
+    /// </summary>
+    public class GreetingState
+    {
+        public string Name { get; set; }
+
+        public string City { get; set; }
+    }
+}
---
+++ ppt-expert-connect/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=expertconnect-test-Web-Deploy.pubxml -p:Password=fusviHWnlyghi86FB4nYx2elivnXGHuBXHjXoleKKCRxHsWSidAndyQvgFRs
+
